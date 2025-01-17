47f6128b6a77a7d1ab038779ef1a678e38a4fc64
abdelwhab Eladawy <abdelwahabeladawy@gmail.com>
2025-01-17 21:56:06 +0200

    fix import of global icon

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,5 +1,5 @@
 import logo from "../../public/assets/icons/logo.svg";
-import global from "../../public/assets/icons/globe.svg";
+import global from "../../public/assets/icons/Globe.svg";
 
 import Toggle from "../components/Toggle";
 import Button from "../components/Button";
@@ -14,7 +14,9 @@ export default function NavBar() {
         </div>
         <ul className=" w-full flex-col justify-center items-center xl:flex xl:flex-row xl:justify-center space-x-10">
           <li className=" text-center my-3 me-7">خدماتنا</li>
-          <li className=" text-center my-3 whitespace-nowrap ">تكنولوجيا المعلومات والأجهزة</li>
+          <li className=" text-center my-3 whitespace-nowrap ">
+            تكنولوجيا المعلومات والأجهزة
+          </li>
           <li className=" text-center my-3">أعمالنا</li>
           <li className=" text-center my-3">من نحن</li>
           <li className=" text-center my-3">الوظائف</li>
@@ -23,13 +25,13 @@ export default function NavBar() {
         <div className="flex justify-center items-center xl:flex xl:flex-row xl:justify-center xl:items-center ">
           <Toggle />
           <span className="flex justify-center items-center w-6 h-6 xl:flex xl:flex-row xl:justify-center xl:items-center">
-          <span className=" xl:me-14  me-6 text-center">English</span>
-            <img src={global} className="w-6 h-6 m-0" alt="globalworld"  />
+            <span className=" xl:me-14  me-6 text-center">English</span>
+            <img src={global} className="w-6 h-6 m-0" alt="globalworld" />
           </span>
 
-          <Button type="primary"><p>اتصل بنا</p></Button>
-
-          
+          <Button type="primary">
+            <p>اتصل بنا</p>
+          </Button>
         </div>
       </div>
     </>
